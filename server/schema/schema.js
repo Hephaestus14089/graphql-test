fd874662980467e790e05e48c71cb9ab719f9975
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-04-26 14:16:02 +0530

    modify schema
    understanding type relations in graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,17 +16,20 @@ let books = [
     { 
         name: "Name of the Wind",
         genre: "Fantasy",
-        id: "1"
+        id: "1",
+        authorId: "1"
     },
     { 
         name: "The Final Empire",
         genre: "Fantasy",
-        id: "2"
+        id: "2",
+        authorId: "2"
     },
     { 
         name: "The Long Earth",
         genre: "Sci-Fi",
-        id: "3"
+        id: "3",
+        authorId: "3"
     }
 ];
 
@@ -57,8 +60,18 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
-    })
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+
+            resolve(parent, args) {
+                // code to get data fron db / other source
+
+                console.log(parent);
+                return _.find(authors, { id: parent.authorId });
+            }
+        } // end of 'author' field
+    }) // end of 'fields'
 });
 
 const AuthorType = new GraphQLObjectType({
