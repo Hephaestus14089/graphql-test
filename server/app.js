8b1b68acf3dd093f0fa116a69fc84d0078c873c2
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2022-06-22 22:21:20 +0530

    connect to mongodb atlas database using mongoose

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+mongoose.connect('mongodb+srv://bhargav:iWjp2xeF3TB9pfV@cluster0.ffpdokd.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log("connection to database establised.");
+});
 
-   schema , // schema: schema 
+app.use('/graphql', graphqlHTTP({
+   schema , // schema: schema
    graphiql: true
 }));
 
